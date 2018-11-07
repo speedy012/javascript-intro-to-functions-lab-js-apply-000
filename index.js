fcd4ce1027ed334d7eac0c7d628e3d757f303a0b
Herman <hrivas5726@aol.com>
2018-11-07 01:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function sayHiToGrandma(string) {
   return 'I can\'t hear you!';
 } if (string.toLowerCase){
 }
+
+function sayHiToGrandma(string) {
+  return 'YES INDEED!'
+} if (string.toUpperCase) {
+}
