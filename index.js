e313178ff3d2d7564b5104dc90c0a925e86653e9
Herman <hrivas5726@aol.com>
2018-11-07 02:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return 'I can\'t hear you!';
+  return 'YES INDEES' === 'I can\'t hear you!';
 } if (string.toLowerCase) {
 }
 
