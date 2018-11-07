2040e94d71f784402eb449435559997cf9e9891a
Herman <hrivas5726@aol.com>
2018-11-07 01:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return 'I can\'t hear you!';
-} if (string.toLowerCase()){
+} if (string.toLowerCase) {
 }
 
 function sayHiToGrandma(string) {
