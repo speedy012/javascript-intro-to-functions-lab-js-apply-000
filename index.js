b6f873b41f32705049e36dadf2d57b7ce813e3b1
Herman <hrivas5726@aol.com>
2018-11-07 01:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-   return string.toUpperCase();
-  })
-})
-
-
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
+function shout(string) {
+  return string.toUpperCase();
+}
