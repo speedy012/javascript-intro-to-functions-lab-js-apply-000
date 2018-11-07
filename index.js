1e9e069c3d605e53c4755fec19f5336d1d57add1
Herman <hrivas5726@aol.com>
2018-11-07 02:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-function shout(string) {
-  return string.toUpperCase();
-}
-
-function whisper(string) {
-  return string.toLowerCase();
-}
-
-function logShout(string) {
-  console.log(string.toUpperCase());
-}
-
-function logWhisper(string) {
-  console.log(string.toLowerCase());
-}
-
-function sayHiToGrandma(string) {
-  return 'I can\'t hear you!'
-} if ('I love you, too') {
-}
-
-function sayHiToGrandma(string) {
-  return 'YES INDEED!'
-} if (string.toUpperCase) {
-}
-
-function sayHiToGrandma(string) {
-  return 'I love you, too.';
-} if ("I Love you, Grandma.") {
-  
-}
 
 
 
